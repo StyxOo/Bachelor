cd10fce2ee259efc81238053f67babade7674c39
StyxOo <l.rothenhaeusler@gmail.com>
2022-04-02 02:19:47 +0200

    More progress

--- project/data/data_crawl.py
+++ project/data/data_crawl.py
@@ -47,4 +47,33 @@ if __name__ == '__main__':
 
         # print_countries(data)
         # print_topics(data)
-        print_topics_for_country(data, "UKR: Ukraine")
+        # print_topics_for_country(data, "UKR: Ukraine")
+
+        # Get topics by country
+        # and get countries by topic
+        by_country = {}
+        by_topic = {}
+        for line in data:
+            country = line[1]
+            topic = line[2]
+            if country in by_country.keys():
+                if topic not in by_country[country]:
+                    by_country[country].append(topic)
+            else:
+                by_country[country] = [topic]
+
+            if topic in by_topic.keys():
+                if country not in by_topic[topic]:
+                    by_topic[topic].append(country)
+            else:
+                by_topic[topic] = [country]
+
+        # Filter topics by country "Ukraine"
+        country = "UKR: Ukraine"
+        possible_topics = by_country[country]
+        for topic in possible_topics:
+            print(f"There are {len(by_topic[topic])} countries for topic: {topic.split(':', 1)[1]}")
+
+    print("Fin")
+
+
