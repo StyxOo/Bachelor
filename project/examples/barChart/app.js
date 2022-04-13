bd63eeafe547c4bf49166e4748af1c0209e38d34
StyxOo <l.rothenhaeusler@gmail.com>
2022-04-13 22:34:34 +0200

    Axes

--- project/examples/barChart/app.js
+++ project/examples/barChart/app.js
@@ -10,7 +10,7 @@ const render = data => {
     const margin = {
         top:20,
         right: 50,
-        bottom: 20,
+        bottom: 50,
         left: 80
     }
     const innerWidth = width - margin.left - margin.right
@@ -28,22 +28,48 @@ const render = data => {
     const g = svg.append('g')
         .attr('transform', `translate(${margin.left},${margin.top})`);
 
-    g.append('g').call(d3.axisLeft(yScale));
-    g.append('g').call(d3.axisBottom(xScale))
+    const xAxisTickFormat = number =>
+        d3.format('.3s')(number)
+            .replace('G', 'B');
+
+    const xAxis = d3.axisBottom(xScale)
+        .tickFormat(xAxisTickFormat)
+        .tickSize(-innerHeight);
+
+    g.append('g')
+        .call(d3.axisLeft(yScale))
+        .selectAll('.domain, .tick line')
+            .remove();
+
+    const xAxisGroup = g.append('g').call(xAxis)
         .attr('transform', `translate(0,${innerHeight})`);
+
+    xAxisGroup
+        .select('.domain')
+            .remove();
+
+    xAxisGroup.append('text')
+        .attr('id', 'axis-label')
+        .attr('fill', 'black')
+        .attr('x', innerWidth / 2)
+        .attr('y', 40)
+        .text('Population')
 
     g.selectAll('rect').data(data)
         .enter().append('rect')
             .attr('width', d => xScale(xValue(d)))
             .attr('height', yScale.bandwidth())
             .attr('y', d => yScale(yValue(d)));
+
+    g.append('text')
+        .text('Country Population')
 };
 
 // .csv creates a promise, when it resolves .then do something else
 d3.csv('data.csv').then(data => {
     data.forEach(d => { // Foreach datapoint in data
         d.population = +d.population * 1000; // Cast value to float and take times 1000
-        d.country = `${d.population}`; // Kind of redundant, but fixed webstorm complaining
+        d.country = `${d.country}`; // Kind of redundant, but fixed webstorm complaining
     });
     console.log(data);
     render(data);
